8a9b8d9ed397830a56dcd2d761773cb73924bf2d
Alexx Roche <github@alexx.net>
2022-09-29 12:56:25 +0100

    Restore the curstor
    Also have `cargo fmt` skip the ascii digits that are in a nice human readable layout

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use chrono::{Local};
+// for hook
+use signal_hook::{iterator::Signals, SIGHUP, SIGINT, SIGQUIT, SIGTERM};
+use std::error::Error;
+use std::thread;
+use std::process::exit;
 
+#[rustfmt::skip]
 // https://www.w3.org/TR/xml-entity-names/025.html
 const DIGITS : [[&str; 11]; 7] = [
     ["┏━┓ ","  ╻  "," ┏━┓ ", " ┏━┓ "," ╻ ╻ "," ┏━┓ "," ┏   "," ┏━┓ "," ┏━┓ "," ┏━┓ ","   "],
@@ -11,11 +17,26 @@ const DIGITS : [[&str; 11]; 7] = [
     ["┗━┛ ","  ╹  "," ┗━━ ", " ┗━┛ ","   ╹ "," ┗━┛ "," ┗━┛ ","   ╹ "," ┗━┛ "," ┗━┛ ","   "],
 ];
 
-
+/// hook to trap signals
+pub fn hook() -> Result<(), Box<dyn Error>> {
+    let signals = Signals::new(&[SIGINT, SIGHUP, SIGTERM, SIGQUIT])?;
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            // return the cursor
+            print!("\x1b[?25h");
+            // exit with the signal
+            exit(sig);
+        }
+    });
+    Ok(())
+}
 
 fn main() {
+    // hide the cursor
     print!("\x1b[2J");
     print!("\x1b[?25l");
+    // hook in the signal listener
+    let _ = hook();    
     loop {
         let t = Local::now();
         let time = t.format("%H:%M:%S").to_string();
